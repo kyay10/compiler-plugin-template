1b1a97fbc7d4a6b94548b774aa7fb325f721869c
Youssef Shoaib <canonballt@gmail.com>
2025-10-22 14:48:53 +0100

    Deduplicate extension config from ExtensionRegistrarConfigurator

--- compiler-plugin/test-fixtures/org/jetbrains/kotlin/compiler/plugin/template/services/ExtensionRegistrarConfigurator.kt
+++ compiler-plugin/test-fixtures/org/jetbrains/kotlin/compiler/plugin/template/services/ExtensionRegistrarConfigurator.kt
@@ -1,21 +1,16 @@
 package org.jetbrains.kotlin.compiler.plugin.template.services
 
-import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
-import org.jetbrains.kotlin.compiler.plugin.template.SimplePluginRegistrar
-import org.jetbrains.kotlin.compiler.plugin.template.ir.SimpleIrGenerationExtension
+import org.jetbrains.kotlin.compiler.plugin.template.SimplePluginComponentRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrarAdapter
 import org.jetbrains.kotlin.test.model.TestModule
 import org.jetbrains.kotlin.test.services.EnvironmentConfigurator
 import org.jetbrains.kotlin.test.services.TestServices
 
 class ExtensionRegistrarConfigurator(testServices: TestServices) : EnvironmentConfigurator(testServices) {
+    private val registrar = SimplePluginComponentRegistrar()
     override fun CompilerPluginRegistrar.ExtensionStorage.registerCompilerExtensions(
         module: TestModule,
         configuration: CompilerConfiguration
-    ) {
-        FirExtensionRegistrarAdapter.registerExtension(SimplePluginRegistrar())
-        IrGenerationExtension.registerExtension(SimpleIrGenerationExtension())
-    }
+    ) = with(registrar) { registerExtensions(configuration) }
 }
