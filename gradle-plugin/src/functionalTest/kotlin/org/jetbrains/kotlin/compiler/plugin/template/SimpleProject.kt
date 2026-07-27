aad9e8132a93974583eb67a946e24e1c5d9cf71c
Youssef Shoaib <canonballt@gmail.com>
2026-07-27 13:49:58 +0100

    Add mavenLocal to gradle-plugin functionalTest

--- gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/compiler/plugin/template/SimpleProject.kt
+++ gradle-plugin/src/functionalTest/kotlin/org/jetbrains/kotlin/compiler/plugin/template/SimpleProject.kt
@@ -1,6 +1,8 @@
 package org.jetbrains.kotlin.compiler.plugin.template
 
 import com.autonomousapps.kit.gradle.Plugin
+import com.autonomousapps.kit.gradle.Repositories
+import com.autonomousapps.kit.gradle.Repository
 import org.jetbrains.kotlin.compiler.plugin.devkit.test.AbstractDevKitGradleProject
 import org.jetbrains.kotlin.compiler.plugin.devkit.test.pluginUnderTestVersion
 
@@ -17,4 +19,8 @@ open class SimpleProject(multiplatform: Boolean = false) :
     // TODO provide in build config
     override val pluginUnderTest: Plugin
         get() = Plugin("org.jetbrains.kotlin.compiler.plugin.template", pluginUnderTestVersion)
+
+    override fun repositories(defaults: List<Repository>): Repositories {
+        return super.repositories(defaults + Repository.MAVEN_LOCAL)
+    }
 }
